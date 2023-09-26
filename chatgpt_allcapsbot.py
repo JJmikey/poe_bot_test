51ec957f4c3775f9dd1905e26288569473d6db33
JJmikey <144325604+JJmikey@users.noreply.github.com>
2023-09-26 06:27:30 +0000

    update requirements.txt

--- chatgpt_allcapsbot.py
+++ chatgpt_allcapsbot.py
@@ -3,10 +3,8 @@
 Sample bot that wraps ChatGPT but makes responses use all-caps.
 
 """
-from __future__ import annotations
 
-from typing import AsyncIterable
-
+from fastapi import FastAPI
 from fastapi_poe import PoeBot
 from fastapi_poe.client import stream_request
 from fastapi_poe.types import (
@@ -16,7 +14,6 @@ from fastapi_poe.types import (
     SettingsResponse,
 )
 
-
 class ChatGPTAllCapsBot(PoeBot):
     async def get_response(self, query: QueryRequest) -> AsyncIterable[PartialResponse]:
         async for msg in stream_request(query, "ChatGPT", query.access_key):
@@ -24,3 +21,17 @@ class ChatGPTAllCapsBot(PoeBot):
 
     async def get_settings(self, setting: SettingsRequest) -> SettingsResponse:
         return SettingsResponse(server_bot_dependencies={"ChatGPT": 1})
+
+fastapi_app = FastAPI()
+
+@fastapi_app.get("/")
+def read_root():
+    return {"Hello": "World"}
+
+@fastapi_app.post("/chatgpt_allcapsbot")
+async def chatgpt_allcapsbot_endpoint(query: QueryRequest):
+    bot = ChatGPTAllCapsBot()
+    responses = []
+    async for response in bot.get_response(query):
+        responses.append(response)
+    return responses
